bdac9223461a7fdb26c1312953586dfd48c4d311
yafiesetyo <muhammad.wibowo@amartha.com>
2022-09-13 11:19:36 +0700

    minor

--- run.sh
+++ run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 curl --location --request POST 'your_test_url' \
-  --header 'Content-Type: application/json'
-#set header here
---data-raw '{your_data}' && echo "job done 1" &
+  --header 'Content-Type: application/json' \
+  --data-raw '{your_data}' && echo "job done 1" &
 
+# next execution
 curl --location --request POST 'your_test_url' \
-  --header 'Content-Type: application/json'
-#set header here
---data-raw '{your_data}' && echo "job done 2" &
+  --header 'Content-Type: application/json' \
+  --data-raw '{your_data}' && echo "job done 2" &
 
 wait
